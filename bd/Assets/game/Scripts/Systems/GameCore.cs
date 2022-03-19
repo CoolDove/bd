12866a049facb0d56510dc25cd809b3ee3bb7e6a
Dove <1515098414@qq.com>
2022-03-19 12:33:02 +0800

    init proj

---
+++ bd/Assets/game/Scripts/Systems/GameCore.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using dove;
+
+namespace game 
+{
+[GameSystem(-100)]
+public class GameCore : IGameSystem
+{
+    public GameMain Game { get; set; }
+
+    public void OnInit() {
+    }
+    public void OnRelease() {
+    }
+}
+}
